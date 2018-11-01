6054617766d766976c8d190546d56b6c142bac95
Jim Pivarski <jpivarski@gmail.com>
2018-11-01 15:51:51 -0500

    round trip from buffer

--- python/histos/interface.py
+++ python/histos/interface.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import flatbuffers
+
 import histos.histos_generated
 import histos.histos_generated.Book
 
@@ -11,15 +12,11 @@ class Book(object):
         builder.Finish(book)
         return builder.Output()
 
-    def _toflatbuffers(self, builder):
-        identifier = builder.CreateString(self._identifier)
-        if self._title is not None:
-            title = builder.CreateString(self._title)
-        histos.histos_generated.Book.BookStart(builder)
-        histos.histos_generated.Book.BookAddIdentifer(builder, identifier)
-        if self._title is not None:
-            histos.histos_generated.Book.BookAddTitle(builder, title)
-        return histos.histos_generated.Book.BookEnd(builder)
+    @classmethod
+    def frombuffer(cls, buffer, offset=0):
+        out = cls.__new__(cls)
+        out._flatbuffers = histos.histos_generated.Book.Book.GetRootAsBook(buffer, offset)
+        return out
 
     def __init__(self, identifier, title=None):
         self.identifier = identifier
@@ -27,6 +24,8 @@ class Book(object):
 
     @property
     def identifier(self):
+        if not hasattr(self, "_identifier"):
+            self._identifier = self._flatbuffers.Identifier()
         return self._identifier
 
     @identifier.setter
@@ -35,8 +34,20 @@ class Book(object):
 
     @property
     def title(self):
+        if not hasattr(self, "_title"):
+            self._title = self._flatbuffers.Title()
         return self._title
 
     @title.setter
     def title(self, value):
         self._title = value
+
+    def _toflatbuffers(self, builder):
+        identifier = builder.CreateString(self._identifier)
+        if self._title is not None:
+            title = builder.CreateString(self._title)
+        histos.histos_generated.Book.BookStart(builder)
+        histos.histos_generated.Book.BookAddIdentifier(builder, identifier)
+        if self._title is not None:
+            histos.histos_generated.Book.BookAddTitle(builder, title)
+        return histos.histos_generated.Book.BookEnd(builder)
