6b99fb752c54c0e0ef6875768fee8b7020e5cc2c
Jim Pivarski <jpivarski@gmail.com>
2018-11-01 22:39:53 -0500

    file works

--- python/histos/interface.py
+++ python/histos/interface.py
@@ -30,8 +30,10 @@ class Book(object):
         return cls.frombuffer(array)
 
     def tofile(self, file, internalize=False):
-        if isinstance(file, str):
+        opened = False
+        if not hasattr(file, "write"):
             file = open(file, "wb")
+            opened = True
 
         if not hasattr(file, "tell"):
             class FileLike(object):
@@ -41,18 +43,38 @@ class Book(object):
                 def write(self, data):
                     self.file.write(data)
                     self.offset += len(data)
+                def close(self):
+                    try:
+                        self.file.close()
+                    except:
+                        pass
                 def tell(self):
                     return self.offset
             file = FileLike(file)
 
         try:
             file.write(b"hist")
-            
-
-
+            builder = flatbuffers.Builder(1024)
+            builder.Finish(self._toflatbuffers(builder, False, file))
+            offset = file.tell()
+            file.write(builder.Output())
+            file.write(struct.pack("<Q", offset))
+            file.write(b"hist")
 
         finally:
-            file.close()
+            if opened:
+                file.close()
+
+    @classmethod
+    def fromfile(cls, file, mode="r+"):
+        if isinstance(file, str):
+            file = numpy.memmap(file, dtype=numpy.uint8, mode=mode)
+        if file[:4].tostring() != b"hist":
+            raise OSError("file does not begin with magic 'hist'")
+        if file[-4:].tostring() != b"hist":
+            raise OSError("file does not end with magic 'hist'")
+        offset, = struct.unpack("<Q", file[-12:-4])
+        return cls.frombuffer(file[offset:-12])
 
     def __init__(self, identifier, title=""):
         self.identifier = identifier
@@ -84,7 +106,7 @@ class Book(object):
     def __repr__(self):
         return "<{0} {1} at 0x{2:012x}>".format(type(self).__name__, repr(self.identifier), id(self))
 
-    def _toflatbuffers(self, builder, out):
+    def _toflatbuffers(self, builder, internalize, file):
         self._valid()
 
         identifier = builder.CreateString(self._identifier)
@@ -94,4 +116,4 @@ class Book(object):
         histos.histos_generated.Book.BookAddIdentifier(builder, identifier)
         if len(self._title) > 0:
             histos.histos_generated.Book.BookAddTitle(builder, title)
-        out[0] = histos.histos_generated.Book.BookEnd(builder)
+        return histos.histos_generated.Book.BookEnd(builder)
