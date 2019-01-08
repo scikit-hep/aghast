443082abbaec0bdcada4c91d6ba39861979133a3
Jim Pivarski <jpivarski@gmail.com>
2019-01-08 07:58:22 -0500

    and actually test it

--- python/tests/test_copy.py
+++ python/tests/test_copy.py
@@ -176,139 +176,185 @@ class Test(unittest.TestCase):
 
     def test_copy_Axis(self):
         h = BinnedEvaluatedFunction([Axis(expression="x", title="wow")], InterpretedInlineBuffer(numpy.array([0.0]), dtype=InterpretedInlineBuffer.float64))
-        assert h == h.copy(checkvalid=True)        
+        assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
 
     def test_copy_UnweightedCounts(self):
         h = Histogram([Axis(RegularBinning(10, RealInterval(-5, 5)))], UnweightedCounts(InterpretedInlineBuffer.fromarray(numpy.arange(10))))
-        assert h == h.copy(checkvalid=True)        
+        assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
 
     def test_copy_WeightedCounts(self):
         h = Histogram([Axis(RegularBinning(10, RealInterval(-5, 5)))], WeightedCounts(InterpretedInlineBuffer.fromarray(numpy.arange(10))))
-        assert h == h.copy(checkvalid=True)        
+        assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
         h = Histogram([Axis(RegularBinning(10, RealInterval(-5, 5)))], WeightedCounts(InterpretedInlineBuffer.fromarray(numpy.arange(10)), sumw2=InterpretedInlineBuffer.fromarray(numpy.arange(10)**2)))
-        assert h == h.copy(checkvalid=True)        
+        assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
         h = Histogram([Axis(RegularBinning(10, RealInterval(-5, 5)))], WeightedCounts(InterpretedInlineBuffer.fromarray(numpy.arange(10)), sumw2=InterpretedInlineBuffer.fromarray(numpy.arange(10)**2), unweighted=UnweightedCounts(InterpretedInlineBuffer.fromarray(numpy.arange(10)))))
-        assert h == h.copy(checkvalid=True)        
+        assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
 
     def test_copy_StatisticFilter(self):
         h = Histogram([Axis(RegularBinning(10, RealInterval(-5, 5)), statistics=Statistics())], UnweightedCounts(InterpretedInlineBuffer.fromarray(numpy.arange(10))))
-        assert h == h.copy(checkvalid=True)        
+        assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
         h = Histogram([Axis(RegularBinning(10, RealInterval(-5, 5)), statistics=Statistics(moments=[Moments(InterpretedInlineBuffer.fromarray(numpy.array([0.0])), 1, filter=StatisticFilter(excludes_nan=False))]))], UnweightedCounts(InterpretedInlineBuffer.fromarray(numpy.arange(10))))
-        assert h == h.copy(checkvalid=True)        
+        assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
 
     def test_copy_Moments(self):
         h = Histogram([Axis(RegularBinning(10, RealInterval(-5, 5)), statistics=Statistics(moments=[Moments(InterpretedInlineBuffer.fromarray(numpy.array([0.0])), 1), Moments(InterpretedInlineBuffer.fromarray(numpy.array([0.0])), 2)]))], UnweightedCounts(InterpretedInlineBuffer.fromarray(numpy.arange(10))))
-        assert h == h.copy(checkvalid=True)        
+        assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
         h = Histogram([Axis(RegularBinning(10, RealInterval(-5, 5)), statistics=Statistics(moments=[Moments(InterpretedInlineBuffer.fromarray(numpy.array([0.0])), 0, weighted=False), Moments(InterpretedInlineBuffer.fromarray(numpy.array([0.0])), 0, weighted=True)]))], UnweightedCounts(InterpretedInlineBuffer.fromarray(numpy.arange(10))))
-        assert h == h.copy(checkvalid=True)        
+        assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
 
     def test_copy_Extremes(self):
         h = Histogram([Axis(RegularBinning(10, RealInterval(-5, 5)), statistics=Statistics(min=Extremes(InterpretedInlineBuffer.fromarray(numpy.array([0.0])))))], UnweightedCounts(InterpretedInlineBuffer.fromarray(numpy.arange(10))))
-        assert h == h.copy(checkvalid=True)        
-        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)        
-        # h = Histogram([Axis(RegularBinning(10, RealInterval(-5, 5)), statistics=Statistics(min=Extremes(InterpretedInlineBuffer.fromarray(numpy.array([0.0]))), max=Extremes(InterpretedInlineBuffer.fromarray(numpy.array([0.0])))))], UnweightedCounts(InterpretedInlineBuffer.fromarray(numpy.arange(10))))
-        # assert h == h.copy(checkvalid=True)        
+        assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
+        h = Histogram([Axis(RegularBinning(10, RealInterval(-5, 5)), statistics=Statistics(min=Extremes(InterpretedInlineBuffer.fromarray(numpy.array([0.0]))), max=Extremes(InterpretedInlineBuffer.fromarray(numpy.array([0.0])))))], UnweightedCounts(InterpretedInlineBuffer.fromarray(numpy.arange(10))))
+        assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
 
     def test_copy_Quantiles(self):
         h = Histogram([Axis(RegularBinning(10, RealInterval(-5, 5)), statistics=Statistics(quantiles=[Quantiles(InterpretedInlineBuffer.fromarray(numpy.array([0.0])), 0.25), Quantiles(InterpretedInlineBuffer.fromarray(numpy.array([0.0]))), Quantiles(InterpretedInlineBuffer.fromarray(numpy.array([0.0])), 0.75)]))], UnweightedCounts(InterpretedInlineBuffer.fromarray(numpy.arange(10))))
-        assert h == h.copy(checkvalid=True)        
+        assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
         h = Histogram([Axis(RegularBinning(10, RealInterval(-5, 5)), statistics=Statistics(quantiles=[Quantiles(InterpretedInlineBuffer.fromarray(numpy.array([0.0])), weighted=False), Quantiles(InterpretedInlineBuffer.fromarray(numpy.array([0.0])), weighted=True)]))], UnweightedCounts(InterpretedInlineBuffer.fromarray(numpy.arange(10))))
-        assert h == h.copy(checkvalid=True)        
-        
+        assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
+
     def test_copy_Modes(self):
         h = Histogram([Axis(RegularBinning(10, RealInterval(-5, 5)), statistics=Statistics(mode=Modes(InterpretedInlineBuffer.fromarray(numpy.array([0.0])))))], UnweightedCounts(InterpretedInlineBuffer.fromarray(numpy.arange(10))))
-        assert h == h.copy(checkvalid=True)        
+        assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
 
     def test_copy_Statistics(self):
         h = Histogram([Axis(RegularBinning(10, RealInterval(-5, 5)), statistics=Statistics()), Axis(RegularBinning(10, RealInterval(-5, 5)), statistics=Statistics())], UnweightedCounts(InterpretedInlineBuffer.fromarray(numpy.arange(100))))
-        assert h == h.copy(checkvalid=True)        
+        assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
         h = Ntuple([Column("one", Column.int32), Column("two", Column.int16)], [NtupleInstance([Chunk([ColumnChunk([Page(RawInlineBuffer(b"\x05\x00\x00\x00"))], [0, 1]), ColumnChunk([Page(RawInlineBuffer(b"\x03\x00"))], [0, 1])])])], column_statistics=[Statistics(moments=[Moments(InterpretedInlineBuffer.fromarray(numpy.array([0.0])), 1), Moments(InterpretedInlineBuffer.fromarray(numpy.array([0.0])), 2)])])
-        assert h == h.copy(checkvalid=True)        
+        assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
 
     def test_copy_Covariance(self):
         h = Histogram([Axis(RegularBinning(10, RealInterval(-5, 5))), Axis(RegularBinning(10, RealInterval(-5, 5)))], UnweightedCounts(InterpretedInlineBuffer.fromarray(numpy.arange(100))), axis_covariances=[Covariance(0, 1, InterpretedInlineBuffer.fromarray(numpy.arange(1)))])
-        assert h == h.copy(checkvalid=True)        
+        assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
         h = Histogram([Axis(RegularBinning(10, RealInterval(-5, 5))), Axis(RegularBinning(10, RealInterval(-5, 5))), Axis(RegularBinning(10, RealInterval(-5, 5)))], UnweightedCounts(InterpretedInlineBuffer.fromarray(numpy.arange(1000))), axis_covariances=[Covariance(0, 1, InterpretedInlineBuffer.fromarray(numpy.arange(1))), Covariance(0, 2, InterpretedInlineBuffer.fromarray(numpy.arange(1))), Covariance(1, 2, InterpretedInlineBuffer.fromarray(numpy.arange(1)))])
-        assert h == h.copy(checkvalid=True)        
+        assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
         h = Histogram([Axis(RegularBinning(10, RealInterval(-5, 5)))], UnweightedCounts(InterpretedInlineBuffer.fromarray(numpy.arange(10))), [Profile("", Statistics([Moments(InterpretedInlineBuffer.fromarray(numpy.zeros(10)), 1), Moments(InterpretedInlineBuffer.fromarray(numpy.zeros(10)), 2)])), Profile("", Statistics([Moments(InterpretedInlineBuffer.fromarray(numpy.zeros(10)), 1), Moments(InterpretedInlineBuffer.fromarray(numpy.zeros(10)), 2)]))], profile_covariances=[Covariance(0, 1, InterpretedInlineBuffer.fromarray(numpy.arange(1)))])
-        assert h == h.copy(checkvalid=True)        
+        assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
         h = Ntuple([Column("one", Column.int32), Column("two", Column.int16)], [NtupleInstance([Chunk([ColumnChunk([Page(RawInlineBuffer(b"\x05\x00\x00\x00"))], [0, 1]), ColumnChunk([Page(RawInlineBuffer(b"\x03\x00"))], [0, 1])])])], column_covariances=[Covariance(0, 1, InterpretedInlineBuffer.fromarray(numpy.arange(1)))])
-        assert h == h.copy(checkvalid=True)        
+        assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
         h = Ntuple([Column("one", Column.int32), Column("two", Column.int16)], [NtupleInstance([Chunk([ColumnChunk([Page(RawInlineBuffer(b"\x05\x00\x00\x00"))], [0, 1]), ColumnChunk([Page(RawInlineBuffer(b"\x03\x00"))], [0, 1])])])], column_covariances=[Covariance(0, 1, InterpretedInlineBuffer.fromarray(numpy.arange(1)), weighted=True), Covariance(0, 1, InterpretedInlineBuffer.fromarray(numpy.arange(1)), weighted=False)])
-        assert h == h.copy(checkvalid=True)        
+        assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
 
     def test_copy_Profile(self):
         h = Histogram([Axis(RegularBinning(10, RealInterval(-5, 5)))], UnweightedCounts(InterpretedInlineBuffer.fromarray(numpy.arange(10))), [Profile("", Statistics([Moments(InterpretedInlineBuffer.fromarray(numpy.zeros(10)), 1), Moments(InterpretedInlineBuffer.fromarray(numpy.zeros(10)), 2)]))])
-        assert h == h.copy(checkvalid=True)        
+        assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
         h = Histogram([Axis(RegularBinning(10, RealInterval(-5, 5))), Axis(RegularBinning(10, RealInterval(-5, 5)))], UnweightedCounts(InterpretedInlineBuffer.fromarray(numpy.arange(100))), [Profile("", Statistics([Moments(InterpretedInlineBuffer.fromarray(numpy.zeros(100)), 1), Moments(InterpretedInlineBuffer.fromarray(numpy.zeros(100)), 2)]))])
-        assert h == h.copy(checkvalid=True)        
-        
+        assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
+
     def test_copy_Histogram(self):
         h = Histogram([Axis()], UnweightedCounts(InterpretedInlineBuffer.fromarray(numpy.arange(1))))
         assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
 
     def test_copy_Parameter(self):
         h = ParameterizedFunction("x**2", [Parameter("x", InterpretedInlineBuffer.fromarray(numpy.array([5]))), Parameter("y", InterpretedInlineBuffer.fromarray(numpy.array([6])))])
         assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
 
     def test_copy_ParameterizedFunction(self):
         h = ParameterizedFunction("x**2")
         assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
         h = Histogram([Axis(RegularBinning(10, RealInterval(-5, 5))), Axis(RegularBinning(10, RealInterval(-5, 5)))], UnweightedCounts(InterpretedInlineBuffer.fromarray(numpy.arange(100))), functions={"f": ParameterizedFunction("x**2", [Parameter("x", InterpretedInlineBuffer.fromarray(numpy.arange(100)))])})
         assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
 
     def test_copy_EvaluatedFunction(self):
         h = Histogram([Axis(RegularBinning(10, RealInterval(-5, 5))), Axis(RegularBinning(10, RealInterval(-5, 5)))], UnweightedCounts(InterpretedInlineBuffer.fromarray(numpy.arange(100))), functions={"f": EvaluatedFunction(InterpretedInlineBuffer.fromarray(numpy.arange(100)))})
         assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
         h = Histogram([Axis(RegularBinning(10, RealInterval(-5, 5))), Axis(RegularBinning(10, RealInterval(-5, 5)))], UnweightedCounts(InterpretedInlineBuffer.fromarray(numpy.arange(100))), functions={"id": EvaluatedFunction(InterpretedInlineBuffer.fromarray(numpy.arange(100)), InterpretedInlineBuffer.fromarray(numpy.arange(100)))})
         assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
         h = Histogram([Axis(RegularBinning(10, RealInterval(-5, 5))), Axis(RegularBinning(10, RealInterval(-5, 5)))], UnweightedCounts(InterpretedInlineBuffer.fromarray(numpy.arange(100))), functions={"id": EvaluatedFunction(InterpretedInlineBuffer.fromarray(numpy.arange(100)), InterpretedInlineBuffer.fromarray(numpy.arange(100)), [Quantiles(InterpretedInlineBuffer.fromarray(numpy.zeros(100)), 0.25), Quantiles(InterpretedInlineBuffer.fromarray(numpy.zeros(100))), Quantiles(InterpretedInlineBuffer.fromarray(numpy.zeros(100)), 0.75)])})
         assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
 
     def test_copy_BinnedEvaluatedFunction(self):
         h = BinnedEvaluatedFunction([Axis()], InterpretedInlineBuffer(numpy.array([0.0]), dtype=InterpretedInlineBuffer.float64))
         assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
         assert frombuffer(h.tobuffer()).values.array.tolist() == [0.0]
         h = BinnedEvaluatedFunction([Axis(), Axis()], InterpretedInlineBuffer(numpy.array([0.0]), dtype=InterpretedInlineBuffer.float64))
         assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
         assert frombuffer(h.tobuffer()).values.array.tolist() == [[0.0]]
 
     def test_copy_Page(self):
         h = Ntuple([Column("one", Column.int32)], [NtupleInstance([Chunk([ColumnChunk([Page(RawInlineBuffer(b"\x05\x00\x00\x00"))], [0, 1])])])])
         assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
         h = Ntuple([Column("one", Column.int32)], [NtupleInstance([Chunk([ColumnChunk([], [0])])])])
         assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
         h = Ntuple([Column("one", Column.int32)], [NtupleInstance([Chunk([ColumnChunk([Page(RawInlineBuffer(b"\x05\x00\x00\x00"))], [0, 1])])])])
         assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
         h = Ntuple([Column("one", Column.int32)], [NtupleInstance([Chunk([ColumnChunk([Page(RawInlineBuffer(b"\x05\x00\x00\x00")), Page(RawInlineBuffer(b"\x04\x00\x00\x00\x03\x00\x00\x00"))], [0, 1, 3])])])])
         assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
 
     def test_copy_Chunk(self):
         h = Ntuple([Column("one", Column.float64)], [NtupleInstance([Chunk([ColumnChunk([], [0])])])])
         assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
         h = Ntuple([Column("one", Column.int32)], [NtupleInstance([])])
         assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
         h = Ntuple([Column("one", Column.int32)], [NtupleInstance([Chunk([ColumnChunk([Page(RawInlineBuffer(b"\x05\x00\x00\x00"))], [0, 1])])])])
         assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
         h = Ntuple([Column("one", Column.int32)], [NtupleInstance([Chunk([ColumnChunk([Page(RawInlineBuffer(b"\x05\x00\x00\x00"))], [0, 1])]), Chunk([ColumnChunk([Page(RawInlineBuffer(b"\x05\x00\x00\x00"))], [0, 1])])])])
         assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
 
     def test_copy_Column(self):
         h = Ntuple([Column("one", Column.float64), Column("two", Column.int32)], [NtupleInstance([])])
         assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
         h = Ntuple([Column("one", Column.int32), Column("two", Column.int16)], [NtupleInstance([Chunk([ColumnChunk([Page(RawInlineBuffer(b"\x05\x00\x00\x00"))], [0, 1]), ColumnChunk([Page(RawInlineBuffer(b"\x03\x00"))], [0, 1])])])])
         assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
 
     def test_copy_Ntuple(self):
         h = Ntuple([Column("one", Column.float64)], [NtupleInstance([])])
         assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
 
     def test_copy_collection(self):
         h = Collection()
         assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
         h = Collection({"b": Collection(), "c": Collection()})
         assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
         h = Collection({"id": Histogram([Axis(RegularBinning(10, RealInterval(-5, 5)))], UnweightedCounts(InterpretedInlineBuffer.fromarray(numpy.arange(30)))), "id2": Histogram([Axis(RegularBinning(100, RealInterval(-5, 5)))], UnweightedCounts(InterpretedInlineBuffer.fromarray(numpy.arange(300))))}, axis=[Axis(RegularBinning(3, RealInterval(-1, 1)))])
         assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
         h = Collection({"b": Collection({"c": Histogram([Axis(RegularBinning(10, RealInterval(-5, 5)))], UnweightedCounts(InterpretedInlineBuffer.fromarray(numpy.arange(60)))), "d": Histogram([Axis(RegularBinning(100, RealInterval(-5, 5)))], UnweightedCounts(InterpretedInlineBuffer.fromarray(numpy.arange(600))))}, axis=[Axis(FractionBinning())])}, axis=[Axis(RegularBinning(3, RealInterval(-1, 1)))])
         assert h == h.copy(checkvalid=True)
+        assert h == frombuffer(h.tobuffer()).copy(checkvalid=True)
