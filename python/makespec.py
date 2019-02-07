83df9f05e7ff6463ecfa5ffead3c8545ce611f07
Jim Pivarski <jpivarski@gmail.com>
2019-02-07 10:58:50 -0600

    roles with hardbreaks

--- python/makespec.py
+++ python/makespec.py
@@ -149,8 +149,7 @@ def num(x):
 def formatted(cls, end="\n"):
     out = ["\n\n=== {0}{1}".format(cls.__name__, end)]
 
-    # out.append("[%hardbreaks]")
-    out.append("[role=\"testy\"]")
+    out.append("[role=\"testy\", %hardbreaks]")
     for name, param in signature(cls.__init__).parameters.items():
         if name != "self":
             check = cls._params[name]
@@ -220,8 +219,7 @@ def formatted(cls, end="\n"):
             else:
                 defaultstring = ""
 
-            # u"\u2022{nbsp}" + 
-            out.append("    * {0} *{1}*: {2}{3}".format(required, name, typestring, defaultstring))
+            out.append(u"\u2022{nbsp}" + "{0} *{1}*: {2}{3}".format(required, name, typestring, defaultstring))
 
     return end.join(out)
 
